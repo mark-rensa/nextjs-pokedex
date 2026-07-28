3e08e6b1d65bb67cd57b763df415efcc68861146
Mark Teekman <mark.teekman@gmail.com>
2026-07-28 16:19:30 +0200

    feat(pokedex): Add scrollbar to Pokedex screen

--- src/components/pokedex-screen.tsx
+++ src/components/pokedex-screen.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export function PokedexScreen({ children }: Props): ReactNode {
   return (
-    <div className="pokedex-screen bg-pokedex-screen relative isolate min-h-96 overflow-clip rounded-md p-8 text-black">
+    <div className="pokedex-screen bg-pokedex-screen relative isolate max-h-96 scrollbar-thumb-purple-700 overflow-y-scroll rounded-md p-8 text-black">
       {children}
     </div>
   );
