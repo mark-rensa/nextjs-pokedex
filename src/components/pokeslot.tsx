bb1101da11218903bfad5c3be77f46ee74e6aa7d
Mark Teekman <mark.teekman@gmail.com>
2026-08-10 19:25:05 +0200

    feat(pokeslot): Add styling and make slot a link

--- src/components/pokeslot.tsx
+++ src/components/pokeslot.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import type { ReactNode } from "react";
 import PokemonTypeBadge from "./pokemon-type-badge";
+import Link from "next/link";
 
 type Props = {
   name: string;
@@ -13,24 +14,29 @@ type Props = {
 
 export function PokeSlot({ name, types, image }: Props): ReactNode {
   return (
-    <li className="flex flex-col items-center gap-2">
-      <p className="first-letter:uppercase">{name}</p>
-      <span className="flex flex-wrap justify-center gap-1">
-        {types.map((pokemonType) => (
-          <PokemonTypeBadge key={pokemonType} variant={pokemonType}>
-            {pokemonType}
-          </PokemonTypeBadge>
-        ))}
-      </span>
-      {image && image.src && (
-        <Image
-          src={image.src}
-          alt={image.alt}
-          width={80}
-          height={80}
-          className="order-first"
-        />
-      )}
+    <li>
+      <Link
+        href="/"
+        className="flex flex-col items-center gap-2 rounded-md border-2 border-gray-500 p-4 transition duration-300 hover:bg-amber-100"
+      >
+        <p className="first-letter:uppercase">{name}</p>
+        <span className="flex flex-wrap justify-center gap-1">
+          {types.map((pokemonType) => (
+            <PokemonTypeBadge key={pokemonType} variant={pokemonType}>
+              {pokemonType}
+            </PokemonTypeBadge>
+          ))}
+        </span>
+        {image && image.src && (
+          <Image
+            src={image.src}
+            alt={image.alt}
+            width={80}
+            height={80}
+            className="order-first"
+          />
+        )}
+      </Link>
     </li>
   );
 }
