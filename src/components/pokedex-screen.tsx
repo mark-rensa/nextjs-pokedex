fc178c636caeec3e4991ef533e9d57d42bbcf4eb
Mark Teekman <mark.teekman@gmail.com>
2026-08-31 20:56:15 +0200

    feat(pokedex): Update hover and scrollbar styles

--- src/components/pokedex-screen.tsx
+++ src/components/pokedex-screen.tsx
@@ -1,15 +1,15 @@
 import { ReactNode } from "react";
 
 type Props = {
-  children: ReactNode;
+	children: ReactNode;
 };
 
 export function PokedexScreen({ children }: Props): ReactNode {
-  return (
-    <div className="pokedex-screen bg-pokedex-screen relative isolate overflow-hidden rounded-md text-black">
-      <div className="pokedex-screen-content max-h-96 overflow-y-scroll p-8 scrollbar-thumb-purple-700">
-        {children}
-      </div>
-    </div>
-  );
+	return (
+		<div className="pokedex-screen bg-pokedex-screen relative isolate overflow-hidden rounded-md text-black">
+			<div className="pokedex-screen-content max-h-96 overflow-y-scroll p-8 scrollbar-thumb-pokedex-screen">
+				{children}
+			</div>
+		</div>
+	);
 }
